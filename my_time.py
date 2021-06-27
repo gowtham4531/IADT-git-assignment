17581e20c8a40b49416fcb85de0a71c1ceea76a7
gowtham <gowthamk1105@gmail.com>
2021-06-27 16:08:17 +0000

    modified master code to get both current time and date

--- my_time.py
+++ my_time.py
@@ -1,4 +1,10 @@
-import datetime
-dispaly = datetime.datetime.now()
-print (dispaly.strftime("%I:%M:%S %p"))
+from datetime import datetime
+from datetime import datetime
+
+# datetime object containing current date and time
+now = datetime.now()
+#print("now =", now)
+# dd/mm/YY H:M:S
+dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+print("date and time =", dt_string)
 
